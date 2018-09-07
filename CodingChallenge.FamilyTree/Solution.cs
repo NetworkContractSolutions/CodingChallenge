e064b8b3f3b340769224c53fc6a8d4372cb50e41
bestguessdeveloper <43041941+bestguessdeveloper@users.noreply.github.com>
2018-09-06 19:59:15 -0500

    Add files via upload

--- CodingChallenge.FamilyTree/Solution.cs
+++ CodingChallenge.FamilyTree/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace CodingChallenge.FamilyTree
 {
@@ -6,7 +7,39 @@ namespace CodingChallenge.FamilyTree
     {
         public string GetBirthMonth(Person person, string descendantName)
         {
-            throw new NotImplementedException();
+            //If the passed in name is the same as the person passed in
+            if (person.Name == descendantName)
+            {
+                return GetMonthString(person.Birthday);
+            }
+
+            //Iterate through children
+            foreach (var child in person.Descendants)
+            {
+                if (child.Name == descendantName)
+                {
+                    return GetMonthString(child.Birthday);
+                }
+
+                //Iterate through grandchildren
+                foreach (var grandchild in child.Descendants)
+                {
+                    if (grandchild.Name == descendantName)
+                    {
+                        return GetMonthString(grandchild.Birthday);
+                    }
+                }
+                //Continue iterations for each child in the tree hierarchy
+            }
+            return string.Empty;
+        }
+
+
+        private static string GetMonthString(DateTime birthday)
+        {
+            return new DateTime(birthday.Year, birthday.Month, birthday.Day).ToString("MMMM", CultureInfo.InvariantCulture);
         }
     }
+
+
 }
