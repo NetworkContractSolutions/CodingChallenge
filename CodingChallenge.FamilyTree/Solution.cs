79bbcef1c361840ebb132c12c9d564ae33891f5a
David Seger <davidseger@SegerMBP.local>
2018-09-30 17:36:36 -0500

    Initial solutions to the challenge

--- CodingChallenge.FamilyTree/Solution.cs
+++ CodingChallenge.FamilyTree/Solution.cs
@@ -6,7 +6,29 @@ namespace CodingChallenge.FamilyTree
     {
         public string GetBirthMonth(Person person, string descendantName)
         {
-            throw new NotImplementedException();
+            /*
+             * I don't search nested trees very often, but my first instinct was recursion so I went with it.
+            */
+
+            // Init string to hold the result
+            string result = "";
+
+            // Are we looking at the correct match already?
+            if (String.Equals(person.Name, descendantName))
+            {
+                result = person.Birthday.ToString("MMMM");
+            }
+            else
+            {
+                // Nope, go deeper until we find them
+                foreach (var p in person.Descendants)
+                {
+                    result = GetBirthMonth(p, descendantName);
+                    if (!String.IsNullOrEmpty(result)) break;
+                }
+            }
+
+            return result;
         }
     }
 }
--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -7,7 +7,17 @@ namespace CodingChallenge.PirateSpeak
     {
         public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            // Sort the jumble alphabetically
+            var j = jumble.ToCharArray();
+            Array.Sort(j);
+
+            // Now compare the sorted jumble with sorted versions of each word in the array, return matches
+            return dictionary.Where(w =>
+            {
+                var c = w.ToCharArray();
+                Array.Sort(c);
+                return j.SequenceEqual(c);
+            }).ToArray();
         }
     }
 }
