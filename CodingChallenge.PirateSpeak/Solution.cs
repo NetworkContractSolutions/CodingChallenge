ad4694371ee3f3f41dd1cf3be6ced575769b05c2
Collin Wiginton <collinw196@gmail.com>
2023-11-09 16:48:36 -0600

    adding solutions to problems

--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CodingChallenge.PirateSpeak
@@ -7,7 +8,33 @@ namespace CodingChallenge.PirateSpeak
     {
         public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            //Declare list and array variabes to keep track of words
+            List<string> possibleWords = new List<string>();
+            string[] wordsArray;
+
+            //Sort the characters in jumble alphabetically
+            char[] jumbleArray = jumble.ToCharArray();
+            Array.Sort(jumbleArray);
+            string sortedJumble = new string(jumbleArray);
+
+            //Loop through each word in dictionary
+            foreach (string word in dictionary)
+            {
+                //Sort the word's characters alphabetically
+                char[] wordArray = word.ToCharArray();
+                Array.Sort(wordArray);
+                string sortedWord = new string(wordArray);
+
+                //Check if the sortedWord and sortedJumble match
+                if (sortedWord == sortedJumble)
+                {
+                    possibleWords.Add(word);
+                }
+            }
+
+            //Convert list to array and return
+            wordsArray = possibleWords.ToArray();
+            return wordsArray;
         }
     }
 }
